a99213a8960d375843e372c4bbac28d3879a75e2
Brian Smith <briansmith@rocketcentral.com>
2024-01-08 16:50:24 -0500

    App renders again, added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import dogeLogo from './assets/logo-dogecoin.svg'
 import './App.css'
+import { timer } from './scripts/timer';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let viteURL: string = 'https://vitejs.dev';
+  let awsURL: string = 'https://aws.amazon.com';
+  let coinPaprikaURL: string = 'https://coinpaprika.com';
+
+  // const [count, setCount] = useState(0)
+
+  const [ coin, setCoin ] = useState(0);
+
+  // Example usage:
+  const timerLengthInMilliseconds = 25000; // Replace with your desired timer length
+  
+  const updateCoin = (x) => {
+    timer(x).then((result) => {
+      setCoin(result); // Update the state of coin here
+      console.log(result); // This will be true after the specified time
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+  }
+
+  updateCoin(timerLengthInMilliseconds)
+
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        {/* {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        </a> */}
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={dogeLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Value</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={updateCoin(timerLengthInMilliseconds)}>
+          Fetch Value
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      </div>
+      <div className="card">
+        <p>{coin}</p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Powered by <a href={awsURL}>AWS</a>, 
+        <a href={viteURL}>Vite</a> and 
+        <a href={coinPaprikaURL}>CoinPaprika</a>
       </p>
     </>
-  )
-}
+  );
 
+      }
 export default App
